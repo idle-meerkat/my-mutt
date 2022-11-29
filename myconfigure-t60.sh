4fc9a95c4e6d00c22bcb071646571c9d2b6fd1f5
Volodymyr Boiko <boyko.cxx@gmail.com>
2022-11-29 03:41:19 +0000

    ok

--- myconfigure-t60.sh
+++ myconfigure-t60.sh
@@ -1,1 +1,4 @@
-./configure --with-ssl --with-curses --with-gnutls --enable-pop --enable-imap --enable-smtp --prefix=/home/user/opt/mutt/
+./configure --with-ssl --with-curses --with-gnutls --with-sasl --with-zlib \
+	--with-idn2 --enable-pop --enable-imap \
+	--enable-smtp \
+	--prefix=/home/user/opt/mutt/
